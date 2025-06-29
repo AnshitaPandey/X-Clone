4d5a255a4455e235a89cf7337ad49e01237a804e
AnshitaPandey <150948010+AnshitaPandey@users.noreply.github.com>
2025-06-29 19:33:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Or wherever your main HTML file is
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
